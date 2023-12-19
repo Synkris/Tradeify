9d96c6f9a9878b161918dbb5a133e0b9afb2c809
Kristian Mathiaz <agbo4chris@yahoo.com>
2023-12-19 17:23:01 +0100

    Change Password

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -90,13 +91,41 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseStatusCodePages(async context =>
+{
+	var response = context.HttpContext.Response;
+
+	if (response.StatusCode == (int)HttpStatusCode.Unauthorized)
+		response.Redirect("/Account/Login");
+
+	if (response.StatusCode == (int)HttpStatusCode.NotFound)
+		response.Redirect("/Home/Error");
+
+	if (response.StatusCode == (int)HttpStatusCode.Forbidden)
+		response.Redirect("/Home/Error");
+
+	if (response.StatusCode == (int)HttpStatusCode.BadGateway)
+		response.Redirect("/Home/Error");
+
+	if (response.StatusCode == (int)HttpStatusCode.InternalServerError)
+		response.Redirect("/Home/Error");
+});
+
+app.UseForwardedHeaders();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseCookiePolicy();
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseSession();
+UpdateDatabase(app);
+
+UpdateDatabase(app);
+HangFireConfiguration(app);
+app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
 pattern: "{controller=Home}/{action=Index}/{id?}");
@@ -128,7 +157,7 @@ ServerName = String.Format(
 app.UseHangfireServer(jobServerOptions);
 var severStorage = new SqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection"));
 JobStorage.Current = severStorage;
-app.UseHangfireDashboard("/GGCHangfire", dashboardOptions, severStorage);
+app.UseHangfireDashboard("/TRADEIFYHangfire", dashboardOptions, severStorage);
 }
 public class ExpirationTimeAttribute : JobFilterAttribute, IApplyStateFilter
 {
